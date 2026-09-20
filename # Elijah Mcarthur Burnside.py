e5b9ec3469f4a70f214345dbc890ac2d46bfcb26
Ejmcx <ejmcarthur05@gmail.com>
2026-09-19 21:14:58 -0400

    Add files via upload

---
+++ # Elijah Mcarthur Burnside.py
@@ -0,0 +1,26 @@
+# Elijah Mcarthur Burnside
+# 9/19/2026
+#P1HW1
+
+#caculate exponents 
+
+print("---------exeponets---------")
+print()
+
+base = int(input("enter a base number:  "))
+exponent = int(input("enter a exponent:  "))
+result = base**exponent
+print(base, "raised to the power of", exponent,"is",result,"!!")
+
+# Calculate addition and subtraction
+print("--------Addition and Subtraction--------")
+print()
+
+num1 = int(input("enter a  starting interger:  "))  
+num2 = int(input("enter an interger to add:  "))  
+num3 = int(input("enter an interger to subtract:  "))  
+
+sum_result = num1 + num2
+final_result = sum_result - num3
+
+print(num1, "plus", num2, "minus", num3, "is equal to", final_result, "!!")
