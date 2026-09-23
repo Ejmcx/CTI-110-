70adb82b442e33160999da9905ddad86bd1b0707
Ejmcx <ejmcarthur05@gmail.com>
2026-09-22 21:29:00 -0400

    CTI 110 Assignments

---
+++ P1LAB1McarthurBurnside.py
@@ -0,0 +1,8 @@
+#Elijah Mcarthur
+#p1lab1.py
+# code
+
+first_name = input ("enter your first name:")
+last_name = input ("enter your last name: ")
+
+print("Hello,", first_name, last_name +"!"," Welcome to CTI-110!")
